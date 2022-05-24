f76cc0f2b458ab6c6c76313926bb781e92d50157
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-05-24 11:28:00 +0200

    function computerPlay and playRound added

--- rockPaperScissers.js
+++ rockPaperScissers.js
@@ -1,1 +1,19 @@
-console.log("hello world !!!");
+console.log("welkom to Rock Paper Scissers");
+
+function computerPlay() {
+    let plays = ["Rock", "Paper", "Scissers"];
+    return plays[Math.floor(Math.random()*3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let p = playerSelection.toLowerCase();
+    let c = computerSelection.toLowerCase();
+
+    if(p === c) {
+        return "It's a Tie"
+    }
+    if (p === "rock" && c === "scissers" || p === "paper" && c === "rock" || p === "scissers" && c === "paper") {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+    return "Lose Win! " + computerSelection + " beats " + playerSelection;
+}
