d7a69e66417bf5524fcc2281e316f954a7014861
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-05-24 11:53:54 +0200

    added game function and fixed a typo

--- rockPaperScissers.js
+++ rockPaperScissers.js
@@ -15,5 +15,32 @@ function playRound(playerSelection, computerSelection) {
     if (p === "rock" && c === "scissers" || p === "paper" && c === "rock" || p === "scissers" && c === "paper") {
         return "You Win! " + playerSelection + " beats " + computerSelection;
     }
-    return "Lose Win! " + computerSelection + " beats " + playerSelection;
+    return "You Lose! " + computerSelection + " beats " + playerSelection;
 }
+
+function game() {
+    playerScore = 0;
+
+    for(let i=0; i < 5; i++) {
+        let userInput = prompt("Type Rock, Paper or Scissers");
+        
+        result = playRound(userInput, computerPlay());
+        console.log(result);
+        if (result.toLowerCase().includes("you win")) {
+            playerScore++;
+        }
+        if (result.toLowerCase().includes("you lose")) {
+            playerScore--;
+        }
+    }
+
+    if (playerScore < 0) {
+        return "You Lost this game";
+    }
+    if (playerScore > 0) {
+        return "You Won this game";
+    }
+    return "this game ends in a Tie"
+}
+
+console.log(game())
